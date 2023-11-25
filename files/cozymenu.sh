0462bc802a22ea8c0a5e179478b2d8d43efd2cfb
remittor <remittor@gmail.com>
2023-11-25 15:07:24 +0300

    Initial commit

---
+++ files/cozymenu.sh
@@ -0,0 +1,177 @@
+#!/bin/sh
+#
+# Copyright (C) 2023 remittor
+#
+
+CM_PROGNAME="cozymenu"
+CM_PROGDIR="/usr/share/$CM_PROGNAME"
+CM_LOGPREF=$CM_PROGNAME
+
+cmlog() {
+	logger -t $CM_LOGPREF "$@"
+}
+ 
+cmerr() {
+	logger -t $CM_LOGPREF -p err "$@"
+}
+
+cmdie() {
+	cmerr "$@"
+	echo "========================================================="
+	sleep 1
+	exit 1
+}
+
+cm_sed_path() {
+	local str=$( ( echo $1|sed -r 's/([\$\.\*\/\[\\^])/\\\1/g'|sed 's/[]]/\\]/g' )>&1 )
+	echo "$str"
+}
+
+CM_MENU_ORIG_FN="/usr/share/ucode/luci/dispatcher.uc"
+CM_MENU_HOOK_FN="$CM_PROGDIR/$CM_PROGNAME.uc"
+CM_MENU_HOOK_FUNC="cozymenu_hook"
+
+#############################
+#import { hash, load_catalog, change_catalog, translate, ntranslate, getuid } from 'luci.core';
+#import { revision as luciversion, branch as luciname } from 'luci.version';
+#import { default as LuCIRuntime } from 'luci.runtime';
+#import { urldecode } from 'luci.http';
+#############################
+
+cm_patch_import() {
+	local cmd
+	local xx
+	if [ ! -f "$CM_MENU_ORIG_FN" ]; then
+		return 1
+	fi
+	xx=$( grep -c -F "$CM_MENU_HOOK_FN" "$CM_MENU_ORIG_FN" )
+	if [ "$xx" != "0" ]; then
+		cmlog 'File "'"$( basename $CM_MENU_ORIG_FN )"'" already patched (import)'
+		return 0
+	fi
+	cmd="import { $CM_MENU_HOOK_FUNC } from '$CM_MENU_HOOK_FN';"
+	cmd=$( cm_sed_path "$cmd" )
+	sed -i "/'luci.runtime'/a $cmd" $CM_MENU_ORIG_FN
+	xx=$( grep -c -F "$CM_MENU_HOOK_FN" "$CM_MENU_ORIG_FN" )
+	if [ "$xx" = "0" ]; then
+		cmerr "Fail on patch import into $CM_MENU_ORIG_FN"
+		return 1
+	fi
+	cmlog 'File "'"$( basename $CM_MENU_ORIG_FN )"'" succefully patched (import)'
+	return 0
+}
+
+#############################
+#function menu_json(acl) {
+#	tree ??= build_pagetree();
+#
+#	if (acl)
+#		apply_tree_acls(tree, acl);
+#
+#	return tree;
+#}
+#############################
+
+cm_patch_funcmenu() {
+	local cmd
+	local xx
+	if [ ! -f "$CM_MENU_ORIG_FN" ]; then
+		return 1
+	fi
+	xx=$( grep -c -F "$CM_MENU_HOOK_FUNC(" "$CM_MENU_ORIG_FN" )
+	if [ "$xx" != "0" ]; then
+		cmlog 'File "'"$( basename $CM_MENU_ORIG_FN )"'" already patched (func)'
+		return 0
+	fi
+	cmd="tree = $CM_MENU_HOOK_FUNC(tree);"
+	cmd=$( cm_sed_path "$cmd" )
+	sed -i "/build_pagetree(/a $cmd" $CM_MENU_ORIG_FN
+	xx=$( grep -c -F "$CM_MENU_HOOK_FUNC(" "$CM_MENU_ORIG_FN" )
+	if [ "$xx" = "0" ]; then
+		cmerr "Fail on patch func into $CM_MENU_ORIG_FN"
+		return 1
+	fi
+	cmlog 'File "'"$( basename $CM_MENU_ORIG_FN )"'" succefully patched (func)'
+	return 0
+}
+
+cm_remove_all_hooks() {
+	sed -i "/$CM_MENU_HOOK_FUNC/d" $CM_MENU_ORIG_FN
+}
+
+cm_install_menu_hooks() {
+	cmlog "cm_install_menu_hooks"
+	cm_patch_import
+	[ $? != 0 ] && { cm_remove_all_hooks ; return 1; }
+	cm_patch_funcmenu
+	[ $? != 0 ] && { cm_remove_all_hooks ; return 1; }
+	rm -f /tmp/luci-index*
+	rm -rf /tmp/luci-modulecache
+	luci-reload
+	return 0
+}
+
+cm_remove_menu_hooks() {
+	cmlog "cm_remove_menu_hooks"
+	cm_remove_all_hooks
+	return 0
+}
+
+# ===================================================================
+
+usage() {
+	cat << EOF
+
+Usage:
+ $CM_PROGNAME [options] -- command
+
+Commands:
+start         install cozy menu patches
+stop          remove cozy menu patches
+
+Parameters:
+ -h           show this help and exit
+ -d           show debug messages
+EOF
+}
+
+usage_err() {
+	printf %s\\n "$CM_PROGNAME: $@" >&2
+	usage >&2
+	exit 1
+}
+
+while getopts ":hd" OPT; do
+	case "$OPT" in
+		h)	usage; exit 0;;
+		d)	CM_DEBUG=1;;
+		:)	usage_err "option -$OPTARG missing argument";;
+		\?)	usage_err "invalid option -$OPTARG";;
+		*)	usage_err "unhandled option -$OPT $OPTARG";;
+	esac
+done
+shift $((OPTIND - 1 ))	# OPTIND is 1 based
+
+case "$1" in
+	boot)
+		cmlog "boot"
+		cm_install_menu_hooks
+		exit 0
+		;;
+	start)
+		cmlog "start"
+		cm_install_menu_hooks
+		exit 0
+		;;
+	stop)
+		cmlog "stop"
+		cm_remove_menu_hooks
+		exit 0
+		;;
+	reload)
+		cmlog "reload"
+		exit 0
+		;;
+	*)
+		usage_err "unknown command - $1";;
+esac
---
+++ files/cozymenu.uc
@@ -0,0 +1,120 @@
+// Copyright 2023 remittor <remittor@gmail.com>
+// SPDX-License-Identifier: GPL-2.0-or-later OR MIT
+
+import { open, stat, glob, lsdir, unlink, basename } from 'fs';
+//import { revision as luciversion, branch as luciname } from 'luci.version';
+
+let cm_data_fn = '/usr/share/cozymenu/cozymenu.json';
+let cm_data = "_empty_";
+
+function syslog2(prio, msg) {
+	warn(sprintf("[%s] %s\n", prio, msg));
+}
+
+function dump2file(fn, data) {
+	let fd = open(fn, 'w', 0600);
+	fd.write(data);
+	fd.close();
+}
+
+function read_jsonfile(path, defval) {
+	let rv;
+	try {
+		rv = json(open(path, "r"));
+	}
+	catch (e) {
+		rv = defval;
+	}
+	return rv;
+}
+
+function cozymenu_hook(menu) {
+	//dump2file("/tmp/menu1.json", menu);
+	try {
+		if (!('admin' in menu.children)) {
+			return menu
+		}
+		if (!('children' in menu.children.admin)) {
+			return menu
+		}
+	}
+	catch {
+		return menu;
+	}
+	syslog2("info", "cozy_menu: 001");
+	
+	if (cm_data == "_empty_") {
+		cm_data = read_jsonfile(cm_data_fn, null);
+	}
+	if (type(cm_data) != 'object') {
+		return menu;
+	}
+	
+	for (let nm_name, nm_v in cm_data) {
+		//syslog2("info", sprintf("%s = '%s'", nm_name, nm_v));
+		let nm_title = nm_v.title;
+		let nm_order = nm_v.order;
+		let nm_items = nm_v.items;
+		//syslog2("info", sprintf("cozy_menu: first menu = '%s' '%s' %d", nm_name, nm_title, nm_order));
+		
+		let nm = menu.children.admin.children[nm_name];
+		if (nm != null) {
+			if (nm.action.type != 'firstchild') {
+				syslog2("error", sprintf("cozy_menu: ERROR: incorrect '%s' menu type!", nm_name));
+				return menu;
+			}
+			//syslog2("info", sprintf("cozy_menu: '%s' menu already exist!", nm_name));
+			nm.satisfied = true;
+			nm.title = nm_title;
+			nm.order = nm_order;
+			nm.action.recurse = true;
+		} else {
+			//syslog2("error", "cozy_menu: NAS menu NOT founded!");
+			nm = {
+				satisfied: true,
+				title: nm_title,
+				order: nm_order,
+				action: {
+					type: 'firstchild',
+					recurse: true
+				},
+				children: {}
+			};
+		}		
+		for (let order, v in nm_items) {
+			let k = v[1];
+			//syslog2("info", sprintf("cozy_menu: %d = %s", order, k));
+			//syslog2("info", sprintf("    '%s'", nm.children[k]));
+			if (nm.children[k]) {
+				//submenu already exist. Skip
+				nm.children[k].order = order + 1;
+			} else if (length(v) > 1) {
+				let k1 = v[0];
+				let k2 = v[1];
+				if (menu.children.admin.children[k1]) {
+					if (menu.children.admin.children[k1].children[k2]) {
+						nm.children[k] = menu.children.admin.children[k1].children[k2];
+						nm.children[k].order = order + 1;
+						if (nm.children[k].action.path) {
+							//syslog2("info", sprintf("cozy_menu: key = '%s' orig path = %s", k, nm.children[k].action.path));
+							let p = split(nm.children[k].action.path, "/");
+							//syslog2("info", sprintf("cozy_menu: %s", p));
+							if (length(p) > 2 && p[0] == "admin" && p[1] == k1) {
+								p[1] = nm_name;
+								p[2] = k;
+								nm.children[k].action.path = join("/", p);
+								//syslog2("info", sprintf("cozy_menu: %s = %s (%d)", k, nm.children[k].action.path, length(p)));
+							}
+						}
+						delete menu.children.admin.children[k1].children[k2];
+					}
+				}
+			}
+		}
+		menu.children.admin.children[nm_name] = nm;
+	}
+	//dump2file("/tmp/menu2.json", menu);
+	return menu;
+}
+
+export { cozymenu_hook }; 
